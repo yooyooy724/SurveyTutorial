b49c6436772fe0cd4e408b7fa5ea676a91d50959
yayu game <yayu.devgames@gmail.com>
2025-05-15 16:37:37 +0900

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SurveyModule } from './survey/survey.module';
+import { typeOrmConfig } from './typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmConfig),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    SurveyModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
